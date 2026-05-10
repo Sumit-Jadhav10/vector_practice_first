91828c20f253ae1df026ee62a9d37306eb26cb7f
Sumit Jadhav <sumit10102006@gmail.com>
2026-05-10 18:03:57 +0530

    this is the code of reverce od array of vector using function

--- 01_vector.cpp
+++ 01_vector.cpp
@@ -15,21 +15,47 @@
 // }
 
 
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int> vac(5);
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cout<<"Enter the "<<i+1<<" number :";
+//         cin >> vac[i];
+//     }
+//     int size = vac.size();
+//     int start = 0;
+//     int end = size - 1;
+//     for (int i = 0; i < size; i++)
+//     {
+//         while (start < end)
+//         {
+//             swap(vac[start], vac[end]);
+//             start++;
+//             end--;
+//         }
+//     }
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << vac[i];
+//     }
+//     return 0;
+// }
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
-int main()
-{
-    vector<int> vac(5);
-    for (int i = 0; i < 5; i++)
-    {
-        cout<<"Enter the "<<i+1<<" number :";
-        cin >> vac[i];
-    }
-    int size = vac.size();
+
+int reverce(vector<int>& vac,int size){
+
+    
     int start = 0;
     int end = size - 1;
-    for (int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
     {
         while (start < end)
         {
@@ -37,12 +63,22 @@ int main()
             start++;
             end--;
         }
+    }      
+}
+int main()
+{
+    vector<int> vac(5);
+    int size = vac.size();
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"Enter the "<<i+1<<" number :";
+        cin >> vac[i];
     }
+    reverce(vac,size);
     for (int i = 0; i < size; i++)
     {
-        cout << vac[i];
+        cout << vac[i]<<" ";
     }
     return 0;
 }
-
-
