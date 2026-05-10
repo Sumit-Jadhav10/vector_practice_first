de1dd5717e85d8288949b0786c38a67a2de72df3
Sumit Jadhav <sumit10102006@gmail.com>
2026-05-10 17:30:09 +0530

    first vector code

---
+++ 01_vector.cpp
@@ -0,0 +1,45 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//  vector<int> vac(5);
+//  for (int i = 0; i < 5; i++)
+//  {
+//     cin>>vac[i];
+//  }
+//  for (int i = 0; i < 5; i++)
+//  {
+//     cout<<vac[i];
+//  }
+
+// }
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> vac(5);
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"Enter the "<<i+1<<" number :";
+        cin >> vac[i];
+    }
+    int size = vac.size();
+    int start = 0;
+    int end = size - 1;
+    for (int i = 0; i < size; i++)
+    {
+        while (start < end)
+        {
+            swap(vac[start], vac[end]);
+            start++;
+            end--;
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << vac[i];
+    }
+    return 0;
+}
