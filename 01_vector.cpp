29ed1ff0d90fa83063366ec63028a60449261c62
Sumit Jadhav <sumit10102006@gmail.com>
2026-05-12 16:17:52 +0530

    pair sum question by using optimal

--- 01_vector.cpp
+++ 01_vector.cpp
@@ -148,34 +148,72 @@
 //     return 0;
 // }
 
+// #include <vector>
+// #include <iostream>
+// using namespace std;
+
+//  vector<int> sumoftwo(vector<int> nums,int target)
+//  {
+//     vector<int> ans;
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         for (int j = i+1; j < nums.size(); j++)
+//         {
+//             if (nums[i]+nums[j]==target)
+//             {
+//                 ans.push_back(i);
+//                 ans.push_back(j);
+//                 return ans;
+//             }
+
+//         }
+
+//     }
+//     return ans;
+
+//  }
+// int main(){
+//     vector<int> nums={1,2,3,4,5,6};
+//     int target= 9;
+//     vector<int> ans =sumoftwo(nums,target);
+//     cout<<ans[0]<<","<<ans[1];
+//     return 0;
+// }
+
 #include <vector>
 #include <iostream>
 using namespace std;
 
- vector<int> sumoftwo(vector<int> nums,int target)
- {
+vector<int> sumoftwo(vector<int> nums, int target)
+{
     vector<int> ans;
-    for (int i = 0; i < nums.size(); i++)
+    int n = nums.size();
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
     {
-        for (int j = i+1; j < nums.size(); j++)
+        if (nums[i] + nums[j] > target)
         {
-            if (nums[i]+nums[j]==target)
-            {
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }
 
+            j--;
         }
-
+        else if (nums[i] + nums[j] < target)
+        {
+            i++;
+        }
+        else
+        {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
     }
-    return ans;
-
- }
-int main(){
-    vector<int> nums={1,2,3,4,5,6};
-    int target= 9;
-    vector<int> ans =sumoftwo(nums,target);
-    cout<<ans[0]<<","<<ans[1];
+}
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5, 6};
+    int target = 9;
+    vector<int> ans = sumoftwo(nums, target);
+    cout << ans[0] << "," << ans[1];
     return 0;
 }
