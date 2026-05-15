085a09a4d63ec3ef4199f0cc04914beba7c5d842
Sumit Jadhav <sumit10102006@gmail.com>
2026-05-15 23:10:24 +0530

    this is 05_Product_of_Array_Except_Self

---
+++ 05_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> nums={1,2,3,4,5,3,4,5};
+    int n = nums.size();
+    vector<int> ans(n,1);
+    for (int i = 1; i < n; i++)
+    {
+        ans[i]=ans[i-1]*nums[i-1];
+    }  
+    int suffix=1;
+    for (int  i =n-2; i >=0 ; i--)
+    {
+        suffix*=nums[i+1];
+        ans[i]*=suffix;
+    }
+    
+   cout<<ans[5];
+    
+
+
+    return 0;
+}
