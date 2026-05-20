4bb8e121d0e3eb27d74cf9a1f858c543e64867bc
Sumit Jadhav <sumit10102006@gmail.com>
2026-05-20 22:47:38 +0530

    try2

--- 05_Product_of_Array_Except_Self.cpp
+++ 05_Product_of_Array_Except_Self.cpp
@@ -1,31 +1,3 @@
-// #include <vector>
-// #include <iostream>
-// #include <algorithm>
-// using namespace std;
-// int main()
-// {
-//     vector<int> nums={1,2,3,4,5,3,4,5};
-//     int n = nums.size();
-//     vector<int> ans(n,1);
-//     for (int i = 1; i < n; i++)
-//     {
-//         ans[i]=ans[i-1]*nums[i-1];
-//     }  
-//     int suffix=1;
-//     for (int  i =n-2; i >=0 ; i--)
-//     {
-//         suffix*=nums[i+1];
-//         ans[i]*=suffix;
-//     }
-    
-//    cout<<ans[5];
-    
-
-
-//     return 0;
-// }
-
-
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -33,24 +5,52 @@ using namespace std;
 int main()
 {
     vector<int> nums={1,2,3,4,5,3,4,5};
-
-   int n=nums.size();
-        vector<int> ans(n,1);
-        vector<int> prifix(n,1);
-        vector<int> suffix(n,1);
-        for(int i=1;i<n;i++){
-            prifix[i]=prifix[i-1]*nums[i-1];
-        }
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]*nums[i+1];
-        }
-        for(int i=0;i<n;i++){
-            ans[i]=prifix[i]*suffix[i];
-        } 
-        
-        cout<<ans[5];
+    int n = nums.size();
+    vector<int> ans(n,1);
+    for (int i = 1; i < n; i++)
+    {
+        ans[i]=ans[i-1]*nums[i-1];
+    }  
+    int suffix=1;
+    for (int  i =n-2; i >=0 ; i--)
+    {
+        suffix*=nums[i+1];
+        ans[i]*=suffix;
+    }
+    
+   cout<<ans[5];
     
 
 
     return 0;
 }
+
+
+// #include <vector>
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// int main()
+// {
+//     vector<int> nums={1,2,3,4,5,3,4,5};
+
+//    int n=nums.size();
+//         vector<int> ans(n,1);
+//         vector<int> prifix(n,1);
+//         vector<int> suffix(n,1);
+//         for(int i=1;i<n;i++){
+//             prifix[i]=prifix[i-1]*nums[i-1];
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             suffix[i]=suffix[i+1]*nums[i+1];
+//         }
+//         for(int i=0;i<n;i++){
+//             ans[i]=prifix[i]*suffix[i];
+//         } 
+        
+//         cout<<ans[5];
+    
+
+
+//     return 0;
+// }
